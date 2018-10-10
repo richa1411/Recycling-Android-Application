1e6a8950c54f764c5d08b2ab50b50ce5244a3957
Kallista Lalonde <lalonde1846@saskpolytech.ca>
2018-10-10 16:03:46 -0600

    Added some UnitTest structure for story1d -- Kallista Lalonde & Robyn Frank

---
+++ kymiraAPITest/story1dTests.cs
@@ -0,0 +1,98 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace kymiraAPITest
+{
+    [TestClass]
+    public class Story1dTests
+    {
+        [TestMethod]
+        public void TestPassToDatabase()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestCreateUser()
+        {
+
+        }
+
+        [TestMethod]
+        /**
+         * Tests that a User can be extracted from the database reliably
+         * */
+        public void TestRetrieval()
+        {
+
+        }
+
+        [TestMethod]
+        /**
+         * Will test for boolean response from constructor
+         * */
+        public void TestDataBaseHandler()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestFirstNameField()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestLastNameField()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestDOBField()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestEmailField()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestAddress1Field()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestAddress2Field()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestPostalCodeField()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestProvinceField()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestCityField()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestPasswordField()
+        {
+
+        }
+    }
+}
