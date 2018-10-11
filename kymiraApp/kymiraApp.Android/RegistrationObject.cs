d2439f4d1f26896aa182e145ec60949d891bcc30
Garrett Pyle <pyle5815@saskpolytech.ca>
2018-10-11 14:56:39 -0600

    gp, nick, initial story commit, created a Registration object used for testing, aswell as a unit test for that object

---
+++ kymiraApp/kymiraApp.Android/RegistrationObject.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace kymiraApp.Droid
+{
+    public class RegistrationObject
+    {
+
+
+        private String EmailAddress { get; }
+        private String password { get; }
+        private String PhoneNumber { get; }
+        private String firstName { get; }
+        private String lastName { get; }
+        private String birthDate { get; }
+        private String addressLine1 { get; }
+        private String addressLine2 { get; }
+        private String City { get; }
+        private String province { get; }
+        private String postalCode { get; }
+        private bool checkBox { get; }
+
+
+
+        public RegistrationObject(String EmailAdress, String Password, String PhoneNumber, String firstName, String lastName,
+                                    String birthDate, String addressLine1, String addressLine2, String City, String province, String postalCode,
+                                     bool checkBox)
+        {
+
+            this.EmailAddress = EmailAddress;
+
+        }
+        public RegistrationObject(String EmailAdress, String Password, String PhoneNumber, String firstName, String lastName,
+                                    String birthDate, String addressLine1, String City, String province, String postalCode,
+                                     bool checkBox){}
+
+
+        public Boolean testStuff()
+        {
+            return true;
+        }
+
+
+
+
+
+        
+    }
+}
---
+++ kymiraAppTest/TestRegistrationObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+using kymiraApp.Droid;
+
+namespace kymiraAppTest
+{
+    [TestFixture(Platform.Android)]
+    class TestRegistrationObject
+    {
+
+
+        RegistrationObject obReg;
+
+       [SetUp]
+        public void TestsetUp()
+        {
+            obReg = new RegistrationObject("", "", "", "", "", "", "", "", "", "","", true);
+        }
+
+        [Test]
+        public void TestThatWeCanCreateARegistrationObject()
+        {
+            Assert.IsTrue(obReg.testStuff());
+        }
+
+    }
+}
