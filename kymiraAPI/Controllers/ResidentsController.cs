4d149603beab20f3c95d6208ad39609462616ea4
Robyn Frank <frank7102@saskpolytech.ca>
2018-10-11 13:51:58 -0600

    Created Resident model with controller -- Robyn Frank & Kallista Lalonde

---
+++ kymiraAPI/Controllers/ResidentsController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using kymiraAPI.Models;
+
+namespace kymiraAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Residents")]
+    public class ResidentsController : Controller
+    {
+        private readonly kymiraAPIContext _context;
+
+        public ResidentsController(kymiraAPIContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Residents
+        [HttpGet]
+        public IEnumerable<Resident> GetResident()
+        {
+            return _context.Resident;
+        }
+
+        // GET: api/Residents/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetResident([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var resident = await _context.Resident.SingleOrDefaultAsync(m => m.id == id);
+
+            if (resident == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(resident);
+        }
+
+        // PUT: api/Residents/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutResident([FromRoute] int id, [FromBody] Resident resident)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != resident.id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(resident).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ResidentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Residents
+        [HttpPost]
+        public async Task<IActionResult> PostResident([FromBody] Resident resident)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Resident.Add(resident);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetResident", new { id = resident.id }, resident);
+        }
+
+        // DELETE: api/Residents/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteResident([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var resident = await _context.Resident.SingleOrDefaultAsync(m => m.id == id);
+            if (resident == null)
+            {
+                return NotFound();
+            }
+
+            _context.Resident.Remove(resident);
+            await _context.SaveChangesAsync();
+
+            return Ok(resident);
+        }
+
+        private bool ResidentExists(int id)
+        {
+            return _context.Resident.Any(e => e.id == id);
+        }
+    }
+}
---
+++ kymiraAPI/Models/Resident.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kymiraAPI.Models
+{
+    public class Resident
+    {
+        public int id { get; set; }
+        private string firstName { get; }
+        private string lastName { get; }
+        private string dateOfBirth { get; }
+        private string email { get; }
+        private string phoneNumber { get; }
+        private string address1 { get; }
+        private string address2 { get; }
+        private string postalCode { get; }
+        private string province { get; }
+        private string city { get; }
+        private string password { get; }
+
+        /**
+         * Public constructor of a Resident object. All attributes are required -- email OR phoneNumber is required.
+         */
+        public Resident(string firstName,string lastName, string dateOfBirth, string email, string phoneNumber, string address1, string address2, string postalCode, string province, string city, string password)
+        {
+            //create a new id - start with 0000001
+        }
+
+        public override string ToString()
+        {
+            return "";  //can return id padded with 0s if desired
+        }
+
+    }
+}
