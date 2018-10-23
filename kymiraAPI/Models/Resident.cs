9cf5451ef1b5611341875d2305b8d5ea56d32b39
Taylor Potts <potts6882@saskpolytech.ca>
2018-10-23 16:16:24 -0600

    Recommitting files after cleaning

---
+++ kymiraAPI/Models/Resident.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kymiraAPI.Models
+{
+    public class Resident
+    {
+
+        public int ID { get; set; }
+
+        public string phoneNumber { get; set; }
+
+        public string password { get; set; }
+    }
+}
