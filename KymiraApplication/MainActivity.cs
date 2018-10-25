29136e3c17618c9dc026fc4d172bf4824db518a3
Nick Letwin <letwin6916@saskpolytech.ca>
2018-10-25 13:53:57 -0600

    Coded jsonHandler object, created a simple layout called activity_registration, altered MainActivity.cs to display the new registration layout -NickL&GarrettP

--- KymiraApplication/MainActivity.cs
+++ KymiraApplication/MainActivity.cs
@@ -11,38 +11,38 @@ using Android.Views;
 namespace KymiraApplication
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity, NavigationView.IOnNavigationItemSelectedListener
+    public class MainActivity : AppCompatActivity //, NavigationView.IOnNavigationItemSelectedListener
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            SetContentView(Resource.Layout.activity_main);
+            SetContentView(Resource.Layout.activity_registration);
             Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
             SetSupportActionBar(toolbar);
 
-            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
-            fab.Click += FabOnClick;
+            //FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            //fab.Click += FabOnClick;
 
-            DrawerLayout drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
-            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, Resource.String.navigation_drawer_open, Resource.String.navigation_drawer_close);
-            drawer.AddDrawerListener(toggle);
-            toggle.SyncState();
+            //DrawerLayout drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            //ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, Resource.String.navigation_drawer_open, Resource.String.navigation_drawer_close);
+            //drawer.AddDrawerListener(toggle);
+            //toggle.SyncState();
 
-            NavigationView navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
-            navigationView.SetNavigationItemSelectedListener(this);
+            //NavigationView navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
+            //navigationView.SetNavigationItemSelectedListener(this);
         }
 
         public override void OnBackPressed()
         {
-            DrawerLayout drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
-            if(drawer.IsDrawerOpen(GravityCompat.Start))
-            {
-                drawer.CloseDrawer(GravityCompat.Start);
-            }
-            else
-            {
-                base.OnBackPressed();
-            }
+            //DrawerLayout drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            //if(drawer.IsDrawerOpen(GravityCompat.Start))
+            //{
+                //drawer.CloseDrawer(GravityCompat.Start);
+            //}
+            //else
+            //{
+                //base.OnBackPressed();
+            //}
         }
 
         public override bool OnCreateOptionsMenu(IMenu menu)
@@ -62,13 +62,14 @@ namespace KymiraApplication
             return base.OnOptionsItemSelected(item);
         }
 
-        private void FabOnClick(object sender, EventArgs eventArgs)
-        {
-            View view = (View) sender;
-            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
-                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
-        }
+        //private void FabOnClick(object sender, EventArgs eventArgs)
+        //{
+            //View view = (View) sender;
+            //Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
+                //.SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
+        //}
 
+            /**
         public bool OnNavigationItemSelected(IMenuItem item)
         {
             int id = item.ItemId;
@@ -98,10 +99,10 @@ namespace KymiraApplication
 
             }
 
-            DrawerLayout drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
-            drawer.CloseDrawer(GravityCompat.Start);
-            return true;
-        }
+            //DrawerLayout drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            //drawer.CloseDrawer(GravityCompat.Start);
+            //return true; 
+        } **/
     }
 }
 
--- KymiraApplication/Model/jsonHandler.cs
+++ KymiraApplication/Model/jsonHandler.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using Newtonsoft.Json;
 using System.Text;
 using System.Threading.Tasks;
 using Android.App;
@@ -13,28 +14,64 @@ using Android.Widget;
 
 namespace KymiraApplication.Model
 {
-    class jsonHandler
+    //This class will handle the creation, serialization, deserialzation, sending, and receiving of JSON objects in order for the
+    //android application to communicate with the back end controller.
+    public class jsonHandler
     {
-
+        // The class has a private HttpClient for POST and GET requests
         private HttpClient client;
 
-
+        // Constructor for the jsonHandler accepts a Uri and creates a new HttpClient
         public jsonHandler()
         {
-            
+            this.client = new HttpClient();
         }
 
-        public async Task<String> sendJsonAsync(Registration item)
+        //This method handles sending a serialized Registration json object to the uri specified
+        public async Task<String> sendJsonAsync(Registration item, Uri uri)
         {
+            // Serialize the Registration item into a JSON object
+            var json = JsonConvert.SerializeObject(item);
 
+            // Conver the JSON object to be StringContent
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
 
-            return "";
+            // Create an HttpResponseMessage to hold the response of the HttpClient's POST
+            HttpResponseMessage response = await client.PostAsync(uri, content);
+
+            // If JSON was sent successfully, return that
+            if(response.IsSuccessStatusCode)
+            {
+                return "Registration successful!";
+            }
+            // Else, notify user that it failed
+            else
+            {
+                return "Registration failed";
+            }
         }
 
-
-        public async Task<String> receiveJsonAsync()
+        // This method handles receiving json from the uri specified
+        public async Task<String> receiveJsonAsync(Uri uri)
         {
-            return "";
+            // Create an HttpResponse message to hold the response from the back end
+            HttpResponseMessage response = await client.GetAsync(uri);
+
+            // Check if the message was sent successfully
+            if(response.IsSuccessStatusCode)
+            {
+                //Create a varialbe to contain the response of the response's GET
+                var content = await response.Content.ReadAsStringAsync();
+
+                return content;
+            }
+            // If there were errors receiving the JSON, let the user know
+            else
+            {
+                return "Error receiving data";
+            }
+
+           
         }
         
 
