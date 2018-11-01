e6893fa1a80f5ee9aa34a78be5b0b79b059a2c76
Kallista Lalonde <lalonde1846@saskpolytech.ca>
2018-11-01 14:29:02 -0600

    Added more params to Model stubs -- Kallista Lalonde

--- KymiraApplication/Model/Bin.cs
+++ KymiraApplication/Model/Bin.cs
@@ -15,9 +15,10 @@ namespace KymiraApplication.Model
     class Bin
     {
         int binID;
+        string binName; //user friendly bin name, something like: '[AddressName] Bin 1'
         int addressID;
         int binStatus; //1 -> good, 2 -> blocked, 3 -> Contaminated
-        int pickupFrequency; //weekly, bi-weekly, etc.
-        int pickupDay; //number (1-5) (Monday-Friday)
+        int pickupFrequency; //weekly, bi-weekly, etc. (How many times a week it is picked up)(1/2 = every second week)
+        int[] pickupDay; //number (1-5) (Monday-Friday)
     }
 }
