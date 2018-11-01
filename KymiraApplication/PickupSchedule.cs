4eb080cd1e5d08b6bf3d55d39fa49077468b9215
Robyn Frank <frank7102@saskpolytech.ca>
2018-11-01 16:08:10 -0600

    Initial story commit, added a new activity called PickupSchedule.cs - RobynF&NickL

---
+++ KymiraApplication/PickupSchedule.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace KymiraApplication
+{
+    [Activity(Label = "PickupSchedule")]
+    public class PickupSchedule : Activity
+    {
+        //Declare jsonHandler
+
+        //Declare UI controls
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            
+            //Assign UI Control
+
+            //Call method to populate text view's with collection information
+        }
+
+        //Function that handles generating the text for the collection date text views
+        private async void setCollectionDateText()
+        {
+            //Request collection dates from the API/backend
+
+            //Update text views with collection date information
+        }
+    }
+}
