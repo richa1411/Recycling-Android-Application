eccde683fc137485ef69a50d82932c30ebecd9f0
Kallista Lalonde <lalonde1846@saskpolytech.ca>
2018-11-01 13:58:02 -0600

    Added new models -- Kallista Lalonde & Garret Pyle

---
+++ KymiraApplication/Model/Bin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace KymiraApplication.Model
+{
+    class Bin
+    {
+        int binID;
+        int addressID;
+        int binStatus; //1 -> good, 2 -> blocked, 3 -> Contaminated
+        int pickupFrequency; //weekly, bi-weekly, etc.
+        int pickupDay; //number (1-5) (Monday-Friday)
+    }
+}
---
+++ KymiraApplication/Model/Building.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace KymiraApplication.Model
+{
+    class Building
+    {
+        int addressID;
+        string addressName; //to be compared to AddressLine1 and AddressLine2 in Resident
+        int NBHDID; // neighbourhood number 
+    }
+}
