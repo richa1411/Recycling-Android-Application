477e8cd77a68723a4e86652701b1db0d8f8793b7
Tanner Chornawka <chornawka7503@siast.sk.ca>
2018-11-08 15:46:39 -0600

    Added documentation to unit tests and made it so that we are testing a list of disposable objects and not a list of strings - Tanner

--- KymiraApplication/Model/Disposable.cs
+++ KymiraApplication/Model/Disposable.cs
@@ -21,21 +21,33 @@ namespace KymiraApplication.Model
 
         }
 
-
+        //The name is required to be a valid disposable 
+        //The name must be at least 3 characters long and cannot exceed 15
         [Required(ErrorMessage = "No name is present")]
         [MinLength(3)]
         [MaxLength(15)]
         public string name { get; set; }
 
+        //Desriptions are optional
         public string description { get; set; }
 
+        //An image is required to be displayed on the button
+        //if an image does not exist, set it to a placeholder
         [Required]
         public string imageURL { get; set; }
 
+        //isRecyclable is a boolean in a disposable object that
+        //states whether the item is disposable or not. it is 
+        //required because that is what is used to query for 
+        //the list in the application.
         [Required]
         public bool isRecyclable { get; set; }
+
+        //The end result is optional
         public string endResult { get; set; }
+        //the quanity recycled is optinal 
         public int qtyRecycled { get; set; }
+        //the reason it is or isn't recycalbe is optional 
         public string recycleReason { get; set; }
     }
 }
--- KymiraApplicationTests/Story2bTests.cs
+++ KymiraApplicationTests/Story2bTests.cs
@@ -16,70 +16,75 @@ namespace KymiraApplicationTests
     public class TestRecyclablesList
     {
         Disposable recItem;
-        List<String> recyclables;
+        List<Disposable> recyclables;
         // List<String> nonRecyclables = new List<string> { "People", "knowledge" };
         //Test that the list is created
         public TestRecyclablesList()
         {
-            recyclables = new List<string>();
-            recItem = new Disposable { name = "Plastic", description = "Its Plastic", imageURL = "image1.png", isRecyclable = true, endResult = "Paper is turned into more paper",
+            recyclables = new List<Disposable>();
+            recItem = new Disposable { name = "Plastic", description = null, imageURL = "image1.png", isRecyclable = true, endResult = "Paper is turned into more paper",
             qtyRecycled = 10, recycleReason = "Paper can be re-used"};
-            }
+        }
+
+        //Test for empty list
+        //Input: an empty list
+        //Expectation: an exception that tells the user there was a problem 
         [TestMethod]
         [ExpectedException(typeof(Exception))]
         public void testEmptyList()
         {
-            recyclables = new List<string>();
+            recyclables = new List<Disposable>();
             if (recyclables.Count == 0)
             {
                 throw new Exception("There seems to be a problem with the items at this time");
             }
             Assert.IsTrue(recyclables.Count == 0);
         }
+
+        //Test that a list is not empty
+        //Input: a list with a disposable item
+        //Expectation: The recycables list has a count greater than 0
         [TestMethod]
         public void testNonEmptyList()
         {
-            recyclables = new List<string> { "Plastic", "Paper" };
-
-            Assert.IsTrue(recyclables.Count != 0);
-            Assert.IsTrue(recyclables.Contains("Plastic"));
+            recyclables = new List<Disposable> { recItem };
+            Assert.IsTrue(recyclables.Count > 0);
+            Assert.IsTrue(recyclables.Contains(recItem));
         }
+
+        //Test that an item has no description
+        //Input: a recItem with no description.
+        //Expectation: recItem's description is empty 
         [TestMethod]
         public void testNoDescription()
         {
-            recItem.description = "";
-            Assert.IsTrue(recItem.description == "");   
+            Assert.IsNull(recItem.description);
         }
 
         [TestMethod]
         public void testDescription()
-        {
-           
+        {         
             recItem.description = "It is Paper";
             Assert.IsTrue(recItem.description == "It is Paper");
         }
         [TestMethod]
         public void testNoImage()
         {
-
             recItem.imageURL = "";
             var results = HelperTestModel.Validate(recItem);
             if (results.Count() == 1)
             {
-                Assert.AreEqual("No image", results[0].ErrorMessage);
-
+                Assert.AreEqual("The imageURL field is required.", results[0].ErrorMessage);
                 results.Clear();
-                recItem.imageURL = "placeholder";
+                recItem.imageURL = "/prj2.cosmo/KymiraApplication/Resources/drawable/No_Image.png";
                 results = HelperTestModel.Validate(recItem);
                 Assert.AreEqual(0, results.Count);
-
             }
         }
 
         [TestMethod]
         public void testImage()
         {
-
             recItem.imageURL = "image1.png";
             Assert.IsTrue(recItem.imageURL == "image1.png");
 
@@ -87,53 +92,38 @@ namespace KymiraApplicationTests
             Assert.AreEqual(0, results.Count());
         }
 
-        
-
         [TestMethod]
         public void testNoName()
         {
-
-
             recItem.name = "";
             Assert.IsTrue(recItem.name == "");
-
             var results = HelperTestModel.Validate(recItem);
             Assert.AreEqual(1, results.Count());
-            Assert.AreEqual("No item name is present", results[0].ErrorMessage);
-
-
+            Assert.AreEqual("No name is present", results[0].ErrorMessage);
         }
 
         [TestMethod]
         public void testReyclableStatusExists()
         {
             Assert.IsTrue(recItem.isRecyclable == true);
-
             var results = HelperTestModel.Validate(recItem);
             Assert.AreEqual(0, results.Count());
-
-
         }
 
         [TestMethod]
         public void testRecyclableStatusDoesNotExist()
         {
-
-
             recItem.name = "";
             Assert.IsTrue(recItem.name == "");
 
             var results = HelperTestModel.Validate(recItem);
             Assert.AreEqual(1, results.Count());
-            Assert.AreEqual("No item name is present", results[0].ErrorMessage);
-
-
+            Assert.AreEqual("No name is present", results[0].ErrorMessage);
         }
 
         [TestMethod]
         public void testName()
         {
-
             recItem.name = "Paper";
             Assert.IsTrue(recItem.name == "Paper");
         }
@@ -141,61 +131,43 @@ namespace KymiraApplicationTests
         [TestMethod]
         public void testNoTurnedInto()
         {
-
-
             recItem.endResult = "";
             Assert.IsTrue(recItem.endResult == "");
-
-
         }
 
         [TestMethod]
         public void testTurnedInto()
         {
-
             recItem.endResult = "Paper is turned into more paper";
             Assert.IsTrue(recItem.endResult == "Paper is turned into more paper");
         }
 
-
         [TestMethod]
         public void testNoitemQuantity()
         {
-
-
             recItem.qtyRecycled = 0;
             Assert.IsTrue(recItem.qtyRecycled == 0);
-
-
         }
 
         [TestMethod]
         public void testitemQuanity()
         {
-
             recItem.qtyRecycled = 10;
             Assert.IsTrue(recItem.qtyRecycled == 10);
         }
 
-
         [TestMethod]
         public void testNoRecyclableReason()
         {
-
-
             recItem.recycleReason = "";
             Assert.IsTrue(recItem.recycleReason == "");
-
-
         }
 
         [TestMethod]
         public void testRecyclableReason()
         {
-
             recItem.recycleReason = "Paper can be re-used";
             Assert.IsTrue(recItem.recycleReason == "Paper can be re-used");
         }
-
     }
 }
