1fb110ccaefb41ed77906e475e218b9dbee0c3ac
Taylor Potts <potts6882@saskpolytech.ca>
2018-11-20 14:01:16 -0600

    Worked on unit tests and our validation helper. Added additional code stubs - Taylor & Richa

--- KymiraApplication/ValidationHelper.cs
+++ KymiraApplication/ValidationHelper.cs
@@ -1,29 +1,29 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
-//using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
 
-//namespace KymiraApplication
-//{
-//    //Helper class that works with Data Annotation Validation to validate the Registration object that is sent by the application
-//    public class ValidationHelper
-//    {
-//        //Validate function that actually handles the validation
-//        public static List<ValidationResult> Validate(object model)
-//        {
-//            //Create a list for the validation results
-//            var results = new List<ValidationResult>();
+namespace KymiraApplication
+{
+    //Helper class that works with Data Annotation Validation to validate the Registration object that is sent by the application
+    public class ValidationHelper
+    {
+        //Validate function that actually handles the validation
+        public static List<ValidationResult> Validate(object model)
+        {
+            //Create a list for the validation results
+            var results = new List<ValidationResult>();
 
-//            //Give the validator a validation context
-//            var validationContext = new ValidationContext(model, null, null);
+            //Give the validator a validation context
+            var validationContext = new ValidationContext(model, null, null);
 
-//            //Run the data annotation validation on the object
-//            Validator.TryValidateObject(model, validationContext, results, true);
+            //Run the data annotation validation on the object
+            Validator.TryValidateObject(model, validationContext, results, true);
 
-//            if (model is IValidatableObject) (model as IValidatableObject).Validate(validationContext);
+            if (model is IValidatableObject) (model as IValidatableObject).Validate(validationContext);
 
-//            //Return the list of validation results
-//            return results;
-//        }
-//    }
-//}
+            //Return the list of validation results
+            return results;
+        }
+    }
+}
