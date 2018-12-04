47b3e2d751bd198e0dd7999c858c38498216b29d
Kallista Lalonde <lalonde1846@saskpolytech.ca>
2018-12-04 14:42:11 -0600

    Started stubbing PickupDate Model

---
+++ kymiraAPI/Models/PickupDate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kymiraAPI.Models
+{
+    /**
+     * This model represents the pickup date object which will be sent to the 
+     * app for display when a user requests their next two pickup dates 
+     * */
+    public class PickupDate
+    {
+        public string binAddress;
+
+        public string collectionDate;
+    }
+}
---
+++ kymiraAPITest/story4gTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using kymiraAPI;
+using kymiraAPI.Models;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace kymiraAPITest
+{
+    [TestClass]
+    public class storyg4gTests
+    {
+        //************ FUNCTIONAL TESTS ************
+       
+
+        //Model Tests
+    }
+}
+
