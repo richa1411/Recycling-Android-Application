51ddc22d5b16266ff20dca06f96116627229d011
William McMunn <mcmunn2031@saskpolytech.ca>
2019-01-16 14:14:39 -0600

    Added most of the functionality to the fragment, and started coding the adapter and layout -- Kyle

---
+++ KymiraApplication/AdapterDisposables.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using KymiraApplication.Models;
+
+namespace KymiraApplication
+{
+    class AdapterDisposables : BaseAdapter<Disposable>
+    {
+
+        public List<Disposable> disposablesList;
+        private Context context;
+
+        public AdapterDisposables(Context context, List<Disposable> disposablesList)
+        {
+            this.context = context;
+            this.disposablesList = disposablesList;
+        }
+
+
+        public override Java.Lang.Object GetItem(int position)
+        {
+            return position;
+        }
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            var view = convertView;
+            AdapterDisposablesViewHolder holder = null;
+
+            if (view != null)
+                holder = view.Tag as AdapterDisposablesViewHolder;
+
+            if (holder == null)
+            {
+                holder = new AdapterDisposablesViewHolder();
+                var inflater = context.GetSystemService(Context.LayoutInflaterService).JavaCast<LayoutInflater>();
+                //replace with your item and your holder items
+                //comment back in
+                //view = inflater.Inflate(Resource.Layout.item, parent, false);
+                //holder.Title = view.FindViewById<TextView>(Resource.Id.text);
+                view.Tag = holder;
+            }
+
+
+            //fill in your items
+            //holder.Title.Text = "new text here";
+
+            return view;
+        }
+
+        //Fill in cound here, currently 0
+        public override int Count
+        {
+            get
+            {
+                return 0;
+            }
+        }
+
+    }
+
+    class AdapterDisposablesViewHolder : Java.Lang.Object
+    {
+        //Your adapter views to re-use
+        //public TextView Title { get; set; }
+    }
+}
--- KymiraApplication/Fragments/DisposablesFragment.cs
+++ KymiraApplication/Fragments/DisposablesFragment.cs
@@ -19,6 +19,19 @@ namespace KymiraApplication.Fragments
 {
     public class DisposablesFragment : Fragment
     {
+
+        
+        private static List<Disposable> disposables; // This will store all of the disposable items
+        private static List<Disposable> recItems; // This will store all of the recyclable Items
+        private static List<Disposable> nonRecItems; // This will store all of the non-recycable Items
+
+        private Button btnViewRecyclables; // Button that will display the recyclable items
+        private Button btnViewNonRecyclables; // Button that will display the non-recyclable items
+        private ListView lvDisposables; // Our list view that will display the Disposable items
+
+        private HttpClient client; // This client is used for GET and POST requests
+
+
         public override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -33,18 +46,40 @@ namespace KymiraApplication.Fragments
             // Use this to return your custom view for this Fragment
             // return inflater.Inflate(Resource.Layout.YourFragment, container, false);
 
-            receiveDisposablesAsync();
+            recItems = new List<Disposable>();
+            nonRecItems = new List<Disposable>();
+
+            btnViewNonRecyclables = ()
+
+            // When the view loads, retrieve the disposables list from the database
+            receiveDisposablesAsync(); 
 
             if (disposables.Count != 0)
             {
 
-
+                // If the list has items in it
+                // Validate those items to make sure they work
+                // Create two new lists, one to store recyclables,
+                // one to store non-recyclables.
+                
+                foreach(Disposable disposableItem in disposables)
+                {
+                    if(disposableItem.isRecyclable)
+                    {
+                        recItems.Add(disposableItem);
+                    }
+                    else
+                    {
+                        nonRecItems.Add(disposableItem);
+                    }
+                }
 
 
             }
             else
             {
-
+                // If the disposables list is empty
+                // Notify the user that server can't be reached.
             }
 
             return base.OnCreateView(inflater, container, savedInstanceState);
@@ -52,10 +87,6 @@ namespace KymiraApplication.Fragments
 
 
         }
-
-
-
-        private static List<Disposable> disposables;
 
 
         /**
@@ -72,6 +103,38 @@ namespace KymiraApplication.Fragments
         public async void receiveDisposablesAsync()
         {
             // populate dispsoables list with stuff from api.
+
+            // Instantiate the HTTP Client
+            client = new HttpClient();
+
+            // Grab the Root API URL and the Disposables URL from the Resources.Strings File
+            String urlAPI = Context.Resources.GetString(Resource.String.UrlAPI);
+            String urlDisposables = Context.Resources.GetString(Resource.String.UrlDisposables);
+
+            // Add them together to form the full URI String
+            String uriString = urlAPI + urlDisposables;
+
+            // Create a URI
+            Uri uri = new Uri(uriString, UriKind.Absolute);
+
+            // Make an HTTPResponseMessage
+            HttpResponseMessage response = await client.GetAsync(uri);
+
+            // Check if the message was sent successfully
+            if(response.IsSuccessStatusCode)
+            {
+                // Create a variable which will store the response of the GET
+                var content = await response.Content.ReadAsStringAsync();
+
+                // Deserialize and set this object to our Disposables List
+                disposables = JsonConvert.DeserializeObject<List<Disposable>>(content);
+            }
+            else
+            {
+                throw new Exception("Error reaching the server, please try again later.");
+            }
+
+
 
 
         }
