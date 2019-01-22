3f0e824688bcc98d1ebe159cd70787d2900f2561
Taylor Potts <potts6882@saskpolytech.ca>
2019-01-22 16:02:46 -0600

    Finished layout, and updated the FAQ Fragment.

--- KymiraApplication/Fragments/FAQFragment.cs
+++ KymiraApplication/Fragments/FAQFragment.cs
@@ -19,13 +19,8 @@ namespace KymiraApplication.Fragments
 {
     public class FAQFragment : Fragment
     {
-        private HttpClient client;
-
         //searchBox input field
         private View view;
-        private EditText searchBox;
-        private TextView txtError;
-        private List<string> FAQList;
 
         //Will run when the app is run. It is the initial creation of the fragment
         public override void OnCreate(Bundle savedInstanceState)
@@ -36,17 +31,10 @@ namespace KymiraApplication.Fragments
         //This view will run when the fragment is called
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
-            view = inflater.Inflate(Resource.Layout.faq_layout, container, false);
-
-            searchBox = (EditText)view.FindViewById(Resource.Id.etxtPhone);
-            
-            txtError = (TextView)view.FindViewById(Resource.Id.tvError);
 
             
 
             return view;
-
-
 
         }
     }
