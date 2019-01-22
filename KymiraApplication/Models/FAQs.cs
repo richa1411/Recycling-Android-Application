ee062068fee4632c305c7893f423af6d56a7c915
Taylor Potts <potts6882@saskpolytech.ca>
2019-01-22 14:16:40 -0600

    Wrote the FAQ model class and the searchFAQ model class. the searchFAQ model class just has the string that will be used to search the FAQ database - Taylor

---
+++ KymiraApplication/Models/FAQs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kymiraAPI.Models
+{
+    public class FAQ
+    {
+        public int ID { get; set; }
+
+        //Set required for the question field so the question MUST be populated, if the question field is empty
+        //an error message of "Qustion is Required" will be displayed
+        [Required(ErrorMessage = "Question cannot be empty")]
+
+        public string question { get; set; }
+
+        //answer field is used to authenticate whether the answer exists in the database
+        [Required(ErrorMessage = "Answer cannot be empty")]
+
+        public string answer { get; set; }
+
+        [Required(ErrorMessage = "No available questions")]
+        public List<string> FAQs { get; set; }
+
+    }
+}
