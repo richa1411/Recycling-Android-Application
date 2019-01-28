e155c696ff090fccb055852028cd472be2807832
Kallista Lalonde <lalonde1846@saskpolytech.ca>
2019-01-28 14:50:03 -0600

    Stubbing out adapter and unit tests for precode --Kallista Lalonde & Garrett Pyle

---
+++ KymiraApplication/Adapters/DisposableDetailListAdapter.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Database;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.Lang;
+using KymiraApplication.Models;
+
+namespace KymiraApplication.Adapters
+{
+    class DisposableDetailListAdapter : IExpandableListAdapter
+    {
+        public List<Disposable> disposableList;
+        Context context;
+
+        public DisposableDetailListAdapter(Context context)
+        {
+            this.context = context;
+        }
+
+
+        //public override Java.Lang.Object GetItem(int position)
+        //{
+        //    return position;
+        //}
+
+        //public override long GetItemId(int position)
+        //{
+        //    return position;
+        //}
+
+        //public override View GetView(int position, View convertView, ViewGroup parent)
+        //{
+        //    var view = convertView;
+        //    DisposableDetailListAdapterViewHolder holder = null;
+
+        //    if (view != null)
+        //        holder = view.Tag as DisposableDetailListAdapterViewHolder;
+
+        //    if (holder == null)
+        //    {
+        //        holder = new DisposableDetailListAdapterViewHolder();
+        //        var inflater = context.GetSystemService(Context.LayoutInflaterService).JavaCast<LayoutInflater>();
+        //        //replace with your item and your holder items
+        //        //comment back in
+        //        //view = inflater.Inflate(Resource.Layout.item, parent, false);
+        //        //holder.Title = view.FindViewById<TextView>(Resource.Id.text);
+        //        view.Tag = holder;
+        //    }
+
+
+        //    //fill in your items
+        //    //holder.Title.Text = "new text here";
+
+        //    return view;
+        //}
+
+        public bool AreAllItemsEnabled()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Java.Lang.Object GetChild(int groupPosition, int childPosition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long GetChildId(int groupPosition, int childPosition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetChildrenCount(int groupPosition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public View GetChildView(int groupPosition, int childPosition, bool isLastChild, View convertView, ViewGroup parent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long GetCombinedChildId(long groupId, long childId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long GetCombinedGroupId(long groupId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Java.Lang.Object GetGroup(int groupPosition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long GetGroupId(int groupPosition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public View GetGroupView(int groupPosition, bool isExpanded, View convertView, ViewGroup parent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsChildSelectable(int groupPosition, int childPosition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnGroupCollapsed(int groupPosition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnGroupExpanded(int groupPosition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterDataSetObserver(DataSetObserver observer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnregisterDataSetObserver(DataSetObserver observer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+        
+        public int GroupCount => throw new NotImplementedException();
+
+        public bool HasStableIds => throw new NotImplementedException();
+
+        public bool IsEmpty => throw new NotImplementedException();
+
+        public IntPtr Handle => throw new NotImplementedException();
+    }
+
+    class DisposableDetailListAdapterViewHolder : Java.Lang.Object
+    {
+        //Your adapter views to re-use
+        //public TextView Title { get; set; }
+    }
+}
---
+++ KymiraApplicationUITests/DisposableDetailsTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace KymiraApplicationUITests
+{
+    [TestFixture(Platform.Android)]
+
+    public class DisposableDetailsTests
+    {
+        IApp app;
+        Platform platform;
+
+        public DisposableDetailsTests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+            app.TapCoordinates(100, 100);
+            app.Tap("Disposables List");
+
+        }
+
+        [Test]
+        public void TestThatItemDescriptionDisplaysProperly()
+        {
+            //tap the list item
+            app.Tap("Tin Cans");
+            //check that the title apears
+            app.WaitForElement(c => c.Marked("Description"));
+            //check that the description contents appear
+            app.WaitForElement(c => c.Marked("Tins Cans Description"));
+        }
+
+        [Test]
+        public void TestThatItemRecycleReasonDisplaysProperly()
+        {
+            //tap the list item
+            app.Tap("Tin Cans");
+            //check that the title apears
+            app.WaitForElement(c => c.Marked("Reason"));
+            //check that the recycle reason contents appear
+            app.WaitForElement(c => c.Marked("Tin Cans Reason"));
+        }
+
+        [Test]
+        public void TestThatItemEndResultDisplaysProperly()
+        {
+            //tap the list item
+            app.Tap("Tin Cans");
+            //check that the title apears
+            app.WaitForElement(c => c.Marked("Result"));
+            //check that the end results contents appear
+            app.WaitForElement(c => c.Marked("Tin Cans End Result"));
+        }
+
+        [Test]
+        public void TestThatItemQtyRecycledDisplaysProperly()
+        {
+            //tap the list item
+            app.Tap("Tin Cans");
+            //check that the title apears
+            app.WaitForElement(c => c.Marked("Quantity Recycled"));
+            //check that the qtyRecycled contents appear
+            app.WaitForElement(c => c.Marked("1200"));
+        }
+    }
+}
