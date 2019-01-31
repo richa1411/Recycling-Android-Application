6f5d61b1d6717ea22bf4f6619648a4bb0eaf0d85
Kallista Lalonde <lalonde1846@saskpolytech.ca>
2019-01-31 14:51:30 -0600

    Started stubbing out unit tests --Kallista Lalonde

---
+++ KymiraAdminTests/AdminViewFAQTests.cs
@@ -0,0 +1,36 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KymiraAdminTests
+{
+    [TestClass]
+    public class AdminViewFAQTests
+    {
+        /**
+         * Test that after being deleted the item is no longer in the list
+         * **/
+        [TestMethod]
+        public void TestThatDeleteButtonRemovesListItem()
+        {
+
+        }
+
+        /**
+         * Test to confirm that save button makes changes to the database list
+         * **/
+        [TestMethod]
+        public void TestThatSaveButtonSavesChangesCorrectly()
+        {
+
+        }
+
+        /**
+         * Test to ensure that a FAQ will be editted in the list when the 
+         * item loses focus after being editted
+         * **/
+        [TestMethod]
+        public void TestThatFAQsEditCorrectlyInList()
+        {
+
+        }
+    }
+}
