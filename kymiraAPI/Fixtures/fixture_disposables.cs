4e236df742741e4059d8d457ac345e2bf5f7324c
Garrett Pyle <pyle5815@saskpolytech.ca>
2019-02-05 14:45:47 -0600

    removed list information on the web page, updated fixtures, updated implemtation diagram garrett taylor

--- kymiraAPI/Fixtures/fixture_disposables.cs
+++ kymiraAPI/Fixtures/fixture_disposables.cs
@@ -15,14 +15,15 @@ namespace kymiraAPI.Fixtures
         //** Recyclable Items **/
         public static List<Disposable> obList = new List<Disposable>(new Disposable[] { new Disposable
         {
-          
+
             name = "Cardboard",
             description = "Cardboard Description",
             imageURL = "Cardboard",
             isRecyclable = true,
             recycleReason = "Cardboard Reason",
             endResult = "Cardboard End Result",
-            qtyRecycled = 1000
+            qtyRecycled = 1000,
+            inactive = false
         }, new Disposable
         {
            
@@ -32,7 +33,8 @@ namespace kymiraAPI.Fixtures
             isRecyclable = true,
             recycleReason = "Paper Reason",
             endResult = "Paper End Result",
-            qtyRecycled = 2500
+            qtyRecycled = 2500,
+            inactive = false
         },
             new Disposable
         {
@@ -43,7 +45,8 @@ namespace kymiraAPI.Fixtures
             isRecyclable = true,
             recycleReason = "Tin Cans Reason",
             endResult = "Tin Cans End Result",
-            qtyRecycled = 1200
+            qtyRecycled = 1200,
+            inactive = false
         },
             //Non recycalble items
             new Disposable
@@ -55,7 +58,8 @@ namespace kymiraAPI.Fixtures
             isRecyclable = false,
             recycleReason = "Pizza Reason",
             endResult = "Pizza End Result",
-            qtyRecycled = 0
+            qtyRecycled = 0,
+             inactive = false
         },
             new Disposable
         {
@@ -66,7 +70,8 @@ namespace kymiraAPI.Fixtures
             isRecyclable = false,
             recycleReason = "Orange Peels Reason",
             endResult = "Orange Peels End Result",
-            qtyRecycled = 0
+            qtyRecycled = 0,
+             inactive = false
         },
             new Disposable
         {
@@ -77,7 +82,8 @@ namespace kymiraAPI.Fixtures
             isRecyclable = false,
             recycleReason = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
             endResult = "Candy End Result",
-            qtyRecycled = 0
+            qtyRecycled = 0,
+             inactive = false
         }
         });
 
