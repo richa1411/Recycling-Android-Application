ba789a51168be11ff54c18445e3a26b4d639fee1
Taylor Potts <potts6882@saskpolytech.ca>
2019-02-05 15:47:28 -0600

    Created AdminTest project. For some reason the targeted framework may be causing some issues in seeing the Admin Project, used the disposables tests and modified to use the correct validation helper - Taylor

---
+++ KymiraAdminTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KymiraAdminTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KymiraAdminTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("946b9ef7-72cb-4b51-bac9-ca844df4e4f0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
