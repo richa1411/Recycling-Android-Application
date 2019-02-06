7b1bda414ecf1fe06bc0b2059843915680bfabbf
Tanner Chornawka <chornawka7503@siast.sk.ca>
2019-02-06 13:51:08 -0600

    Created a new MSTest class and installed the selenium packages, started writing UI tests for the page. -Tanner and Kallista

---
+++ KymiraAdministratorTests/TestValidationHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace KymiraAdministratorTests
+{
+    //this validation helper class has a method called validate that will accept Model class's object,
+    //and stores results of validating objects (error messages) in a list
+   public class TestValidationHelper
+    {
+        
+            public static List<ValidationResult> Validate(object model)
+            {
+            var results = new List<ValidationResult>();
+            //initializing validationcontext class, that will check against two objects,
+            //in this case data annotations in Model class
+            var validationContext = new ValidationContext(model, null, null);
+            //validates object of Model
+            Validator.TryValidateObject(model, validationContext, results, true);
+            //stores all validation results in result variable
+            if (model is IValidatableObject) (model as IValidatableObject).Validate(validationContext);
+
+
+            return results;
+        }
+    }
+}
+    
+
