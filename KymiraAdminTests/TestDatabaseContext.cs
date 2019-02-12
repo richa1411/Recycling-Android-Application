b6d25447ead3ff41c411aeefe829daaef44ceeed
Garrett Pyle <pyle5815@saskpolytech.ca>
2019-02-12 10:59:45 -0600

    added TestDatabaseContext file to project. gp

---
+++ KymiraAdminTests/TestDatabaseContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using KymiraAdmin.Models;
+using System;
+
+namespace KymiraAdminTests
+{
+    class TestDatabaseContext : IDisposable
+    {
+        private DbContextOptionsBuilder<KymiraAdminContext> config = new DbContextOptionsBuilder<KymiraAdminContext>();
+        public KymiraAdminContext context { get; private set; }
+
+        public TestDatabaseContext()
+        {
+            config.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=kymiraAPIDatabase29;Trusted_Connection=True;MultipleActiveResultSets=true");
+            context = new KymiraAdminContext(config.Options);
+
+            context.Database.EnsureCreated();
+
+        }
+
+        public void Dispose()
+        {
+            //remove all the data from db
+        }
+    
+
+    }
+}
