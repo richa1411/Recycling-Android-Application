004a5617f65e6ee8c0bd7070af120a1e477d3c6d
Robyn Frank <frank7102@saskpolytech.ca>
2019-02-13 14:59:21 -0600

    Created fixture class and added method stubs for adding and removing data from the BinStatus table to test for viewing -- Robyn Frank

---
+++ KymiraAdmin/Fixtures/fixture_bin_status.cs
@@ -0,0 +1,64 @@
+﻿using KymiraAdmin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KymiraAdmin.Fixtures
+{
+    public class fixture_bin_status
+    {
+        //This class is used to add and delete data from the database for testing which items are displayed to the page.
+
+        //list of BinStatus objects to be added to database
+        public static List<BinStatus> obBins = new List<BinStatus> {
+        new BinStatus
+        {
+            binID = "W114-320-203",
+            siteID = 1609312,
+            status = 1,
+            collectionDate = "2019-01-01"
+        },
+        new BinStatus
+        {
+            binID = "W114-320-204",
+            siteID = 1609312,
+            status = 2,
+            collectionDate = "2019-01-01"
+        },
+        new BinStatus
+        {
+            binID = "W114-320-205",
+            siteID = 1609312,
+            status = 1,
+            collectionDate = "2019-01-01"
+        },
+        new BinStatus
+        {
+             binID = "COSMO123",
+            siteID = 1609320,
+            status = 1,
+            collectionDate = "2019-01-01"
+        },
+        new BinStatus
+        {
+            binID = "12345",
+            siteID = 1609320,
+            status = 3,
+            collectionDate = "2019-01-01"
+        }};
+
+        /* This function will load specific Bin Status objects to the Bin Status table in the database passed in.*/
+        public static void Load(KymiraAdminContext _context)
+        {
+                //load data and save changes
+        }
+
+        /* This function will remove all Bin Status objects in the Bin Status table from the database passed in. */
+        public static void Unload(KymiraAdminContext _context)
+        {
+                //unload data and save changes
+        }
+        
+    }
+}
--- KymiraAdminTests/AdminViewCollectionTests.cs
+++ KymiraAdminTests/AdminViewCollectionTests.cs
@@ -1,13 +1,15 @@
 ﻿using KymiraAdmin.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
+using KymiraAdmin.Fixtures;
 
 namespace KymiraAdminTests
 {
     [TestClass]
     public class AdminViewCollectionTests
     {
-        //list of BinStatuses to compare test results to -- in order (siteID, binID, collectionDate(?))
+        
+        /*
         List<BinStatus> list = new List<BinStatus>
         {
             new BinStatus{ binID = "W114-320-123", collectionDate = "2018-01-01", siteID = 1609312, status = 1 },
@@ -15,40 +17,48 @@ namespace KymiraAdminTests
             new BinStatus{ binID = "W114-320-125", collectionDate = "2018-01-01", siteID = 1609312, status = 1 },
             new BinStatus{ binID = "W114-320-123", collectionDate = "2018-02-02", siteID = 1609313, status = 1 }
         };
+        */
+
+        //list of BinStatuses to compare test results to -- in order (siteID, binID, collectionDate(?))
+        List<BinStatus> dbBins = fixture_bin_status.obBins;
+        
 
         [TestInitialize]
         public void Setup()
         {
             //remove all items from the database and add data to be displayed to test for
-            
+            //call fixture class to load bin statuses
         }
 
         [TestMethod]
         //Test that a deleted collection status is removed from the list (and database)
         public void TestThatDeletedStatusNotDisplayed()
         {
-            
+            //List<BinStatus> list = BinStatusPage.Index();
+            //Assert.IsTrue(!dbBins.Contains());
         }
 
         [TestMethod]
         //test that list is displayed in the correct order
         public void TestThatListIsDisplayedInOrder()
         {
-
+            //Assert.IsTrue(dbBins.Equals());
         }
 
         [TestMethod]
         //test that an empty list (no bin status objects to display) displays message
         public void TestThatEmptyListDisplaysMessage()
         {
-
+            //assert is true that list received is empty
+            //assert is true that message is displayed
         }
 
         [TestMethod]
         //test that message is displayed if connection cannot be made / is timed out
         public void TestThatNoConnectionDisplaysMessage()
         {
-
+            //assert is true that message is displayed
+            //assert is true that list is not displayed
         }
     }
 }
