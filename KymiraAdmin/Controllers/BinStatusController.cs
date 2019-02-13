eba6837c63c48e07197a5b3d15d94ee8ddd6be0d
Robyn Frank <frank7102@saskpolytech.ca>
2019-02-13 13:54:17 -0600

    Created BinStatusController, commented out functions that we are not implementing, reordered layout to be able to navigate to the Collection statuses page and started writing unit tests -- Robyn Frank

---
+++ KymiraAdmin/Controllers/BinStatusController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using KymiraAdmin.Models;
+
+namespace KymiraAdmin.Controllers
+{
+    public class BinStatusController : Controller
+    {
+        //database context object
+        private readonly KymiraAdminContext _context;
+
+        public BinStatusController(KymiraAdminContext context)
+        {
+            _context = context;
+        }
+
+        // GET: BinStatus
+        public async Task<IActionResult> Index()
+        {
+            //return the list of all binstatuses here ---- TODO: sort by siteID then binID
+            return View(await _context.BinStatus.ToListAsync());
+        }
+
+        // GET: BinStatus/Details/5
+        /*
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var binStatus = await _context.BinStatus
+                .SingleOrDefaultAsync(m => m.pickupID == id);
+            if (binStatus == null)
+            {
+                return NotFound();
+            }
+
+            return View(binStatus);
+        }
+
+        // GET: BinStatus/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BinStatus/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("pickupID,binID,status,siteID,collectionDate")] BinStatus binStatus)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(binStatus);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(binStatus);
+        }
+
+        // GET: BinStatus/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var binStatus = await _context.BinStatus.SingleOrDefaultAsync(m => m.pickupID == id);
+            if (binStatus == null)
+            {
+                return NotFound();
+            }
+            return View(binStatus);
+        }
+
+        // POST: BinStatus/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("pickupID,binID,status,siteID,collectionDate")] BinStatus binStatus)
+        {
+            if (id != binStatus.pickupID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(binStatus);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BinStatusExists(binStatus.pickupID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(binStatus);
+        }
+        */
+
+        // GET: BinStatus/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var binStatus = await _context.BinStatus
+                .SingleOrDefaultAsync(m => m.pickupID == id);
+            if (binStatus == null)
+            {
+                return NotFound();
+            }
+
+            return View(binStatus);
+        }
+
+        // POST: BinStatus/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var binStatus = await _context.BinStatus.SingleOrDefaultAsync(m => m.pickupID == id);
+            _context.BinStatus.Remove(binStatus);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BinStatusExists(int id)
+        {
+            return _context.BinStatus.Any(e => e.pickupID == id);
+        }
+    }
+}
---
+++ KymiraAdminTests/AdminViewCollectionTests.cs
@@ -0,0 +1,54 @@
+﻿using KymiraAdmin.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace KymiraAdminTests
+{
+    [TestClass]
+    public class AdminViewCollectionTests
+    {
+        //list of BinStatuses to compare test results to -- in order (siteID, binID, collectionDate(?))
+        List<BinStatus> list = new List<BinStatus>
+        {
+            new BinStatus{ binID = "W114-320-123", collectionDate = "2018-01-01", siteID = 1609312, status = 1 },
+            new BinStatus{ binID = "W114-320-124", collectionDate = "2018-01-01", siteID = 1609312, status = 1 },
+            new BinStatus{ binID = "W114-320-125", collectionDate = "2018-01-01", siteID = 1609312, status = 1 },
+            new BinStatus{ binID = "W114-320-123", collectionDate = "2018-02-02", siteID = 1609313, status = 1 }
+        };
+
+        [TestInitialize]
+        public void Setup()
+        {
+            //remove all items from the database and add data to be displayed to test for
+            
+        }
+
+        [TestMethod]
+        //Test that a deleted collection status is removed from the list (and database)
+        public void TestThatDeletedStatusNotDisplayed()
+        {
+            
+        }
+
+        [TestMethod]
+        //test that list is displayed in the correct order
+        public void TestThatListIsDisplayedInOrder()
+        {
+
+        }
+
+        [TestMethod]
+        //test that an empty list (no bin status objects to display) displays message
+        public void TestThatEmptyListDisplaysMessage()
+        {
+
+        }
+
+        [TestMethod]
+        //test that message is displayed if connection cannot be made / is timed out
+        public void TestThatNoConnectionDisplaysMessage()
+        {
+
+        }
+    }
+}
